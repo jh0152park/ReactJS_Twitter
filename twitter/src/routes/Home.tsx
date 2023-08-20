b779a9fa83cdcde48d20afc5b971d1913b62e43b
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-20 16:05:00 +0900

    read all tweets

--- twitter/src/routes/Home.tsx
+++ twitter/src/routes/Home.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { dbService } from "../firebase";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {
+    addDoc,
+    collection,
+    getDocs,
+    query,
+    serverTimestamp,
+} from "firebase/firestore";
 
 function Home() {
     const [tweet, setTweet] = useState("");
+    const [allTweets, setAllTweets] = useState<any>([]);
 
     async function handleSubmit(event: any) {
         event.preventDefault();
 
         await addDoc(collection(dbService, "tweets"), {
             tweet: tweet,
-            createAt: serverTimestamp(),
+            createAt: Date.now(),
         });
         setTweet("");
     }
@@ -21,6 +28,25 @@ function Home() {
         } = event;
         setTweet(value);
     }
+
+    async function getAllTweets() {
+        const q = query(collection(dbService, "tweets"));
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((document) => {
+            const tweetObj = {
+                ...document.data(),
+                id: document.id,
+            };
+            setAllTweets((prev: any) => [tweetObj, ...prev]);
+        });
+    }
+
+    useEffect(() => {
+        getAllTweets();
+    }, []);
+
+    console.log(allTweets);
 
     return (
         <div>
@@ -35,6 +61,13 @@ function Home() {
                 ></input>
                 <input type="submit" value="Tweet"></input>
             </form>
+            <div>
+                {allTweets.map((tweet: any) => (
+                    <div key={tweet.id}>
+                        <h4>{tweet.tweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
