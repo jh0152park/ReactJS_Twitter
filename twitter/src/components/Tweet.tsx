cbe82e3abeb4ad367a2358c0662ff668282025c0
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-20 18:04:41 +0900

    added update post feature

--- twitter/src/components/Tweet.tsx
+++ twitter/src/components/Tweet.tsx
@@ -1,13 +1,69 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { dbService } from "../firebase";
+import { useState } from "react";
+
 function Tweet({ tweetObj, isOwner }: { tweetObj: any; isOwner: boolean }) {
+    const [edit, setEdit] = useState(false);
+    const [newTweet, setNewTweet] = useState<string>(tweetObj.tweet);
+
+    async function handleDeletePost() {
+        const agree = window.confirm(
+            "Are you sure you want to delete  this post?"
+        );
+        const TweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
+        if (agree) {
+            await deleteDoc(TweetTextRef);
+        }
+    }
+
+    function toggleEdit() {
+        setEdit((prev) => !prev);
+    }
+
+    function handleEditChange(event: any) {
+        const {
+            target: { value },
+        } = event;
+        setNewTweet(value);
+    }
+
+    async function handleSubmit(event: any) {
+        event.preventDefault();
+        const TweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
+
+        await updateDoc(TweetTextRef, {
+            tweet: newTweet,
+        });
+        setEdit(false);
+    }
+
     return (
         <div>
-            <h4>{tweetObj.tweet}</h4>
-            {isOwner ? (
+            {edit ? (
                 <>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            onChange={handleEditChange}
+                            placeholder="What's new on your mind!?"
+                            type="text"
+                            value={newTweet}
+                            required
+                        />
+                        <input type="submit" value="Update Tweet" />
+                    </form>
+                    <button onClick={toggleEdit}>Cancle</button>
                 </>
-            ) : null}
+            ) : (
+                <>
+                    <h4>{tweetObj.tweet}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={toggleEdit}>Edit</button>
+                            <button onClick={handleDeletePost}>Delete</button>
+                        </>
+                    )}
+                </>
+            )}
         </div>
     );
 }
